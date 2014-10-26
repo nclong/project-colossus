93ddfd2b42a6e97891ebf9fed66af8c1ede7ca60
Nick Long <nclong@uci.edu>
2014-10-26 00:40:36 -0700

    Initial Commit

---
+++ project-colossus/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+    [Range(0f, 1.0f)]
+    public float m_movementSpeed;
+
+    [Range( 0f, 1.0f )]
+    public float m_rotationSpeed;
+
+    [Range( 0f, 1.0f )]
+    public float m_inputEpsilon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movementInput = new Vector3( Input.GetAxis( "LeftJoystickX" ), Input.GetAxis( "LeftJoystickY" ), 0f );
+        Vector3 rotationInput = new Vector3( 0f, 0f, Mathf.Atan2(Input.GetAxis( "RightJoystickY" ), Input.GetAxis( "RightJoystickX" )) * Mathf.Rad2Deg - 90.0f);
+        transform.position = Vector3.Lerp( transform.position, transform.position + movementInput, m_movementSpeed );
+        if( !rightJoystickIsNull )
+        {
+            transform.eulerAngles = new Vector3( 0f, 0f, Mathf.LerpAngle( transform.eulerAngles.z, -rotationInput.z, m_rotationSpeed ) );
+        }
+	}
+
+    private bool rightJoystickIsNull
+    {
+        get { return Input.GetAxis( "RightJoystickX" ).IsWithin( 0f, m_inputEpsilon ) && Input.GetAxis( "RightJoystickY" ).IsWithin( 0f, m_inputEpsilon ); }
+    }
+}
---
+++ project-colossus/Assets/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constants {
+
+	public static bool IsWithin( this float x, float target, float epsilon)
+    {
+        return Mathf.Abs( Mathf.Abs( x ) - target ) <= epsilon;
+    }
+}
