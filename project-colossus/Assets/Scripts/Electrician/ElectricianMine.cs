520887114b16aa46b720474bdaefae9817bf4a45
bombguy <cshin3@uci.edu>
2014-12-03 15:23:27 -0800

    Added ElectricianMine.cs  - Added Trigger when Object with "Enemy" Tag collides with mine
    Added ElectricianPlaceMine.cs
     - Electrician can place Mine in game by pressing 1 on keyboard
     - Maximum of 2 Mine can be placed

---
+++ project-colossus/Assets/Scripts/Electrician/ElectricianMine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricianMine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Enemy")
+        {
+            Debug.Log("Enemy Stepped on the Mine");
+            //Show Explosion Animation
+
+            //Do Damage to the enemy
+            //coll.gameObject.
+
+            //Make the mine disappear
+            renderer.enabled = false;
+
+            //Delete from hierarchy
+            Invoke("DestroyMine", 1);
+        }
+        else if (coll.tag == "Player")
+        {
+            Debug.Log("Player Stepped on the Mine");
+        }
+    }
+
+    void DestroyMine()
+    {
+        //Update Number of Mine in game
+        Destroy(gameObject);
+    }
+}
---
+++ project-colossus/Assets/Scripts/Electrician/ElectricianPlaceMine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricianPlaceMine : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject mine;
+
+    public int max_mine = 2;
+    public int current_mine;
+
+	// Use this for initialization
+	void Start () {
+        current_mine = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && current_mine < 2)
+        {
+            Vector3 pos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 5);
+            Instantiate(mine, pos, Quaternion.identity);
+
+            current_mine++;
+            Debug.Log("Placed a Mine");
+        }
+	}
+}
