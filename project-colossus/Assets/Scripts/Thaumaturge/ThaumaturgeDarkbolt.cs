3437749921bd29de03cc11a16b00995a5141339c
Tanthallasa <smithne@uci.edu>
2015-03-02 18:39:20 -0800

    darkbolt and rune circle functional but formatted wrong

--- project-colossus/Assets/Scripts/Thaumaturge/ThaumaturgeDarkbolt.cs
+++ project-colossus/Assets/Scripts/Thaumaturge/ThaumaturgeDarkbolt.cs
@@ -3,4 +3,28 @@ using System.Collections;
 
 public class ThaumaturgeDarkbolt : MonoBehaviour {
 	
+    private static Transform playerPos;
+    public KeyCode key;
+    public GameObject projectile;
+    private GameObject boss;
+
+    // Use this for initialization
+    void Start()
+    {
+        boss = GameObject.FindGameObjectWithTag("EnemyObject");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            GameObject proj = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+            Projectile projComponent = proj.GetComponent<Projectile>();
+            projComponent.SetTargetByDest(boss.transform.position);
+            projComponent.Launch();
+        }
+        playerPos = transform;
+    }
 }
+
--- project-colossus/Assets/Scripts/Thaumaturge/ThaumaturgePlaceRuneCircle.cs
+++ project-colossus/Assets/Scripts/Thaumaturge/ThaumaturgePlaceRuneCircle.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class ThaumaturgePlaceRuneCircle : MonoBehaviour {
 
     private static Transform playerPos;
+    public KeyCode key;
+    public GameObject rune;
+    public bool runePlaced = false;
     public static GameObject PlaceRune(GameObject rune, IThaumaturgeAbility ability)
     {
         ability.runePlaced = true;
         return (GameObject)Instantiate( rune, playerPos.position, playerPos.rotation );
         
     }
+
 
     // Use this for initialization
     void Start()
@@ -20,6 +24,12 @@ public class ThaumaturgePlaceRuneCircle : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetKeyDown(key))
+            if (!runePlaced)
+            {
+                runePlaced = true;
+                Instantiate(rune, transform.position, Quaternion.identity);
+            }
         playerPos = transform;
     }
 }
