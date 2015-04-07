365fc034ec9d3d003c388065c5360ec71bf8c6d9
unknown <akasiri@uci.edu>
2015-04-07 02:22:36 -0700

    Arzang commiting my scene, ElecCharge, ElecTowerRescource. Prayhope I don't break this ;_;

--- project-colossus/Assets/Scripts/Electrician/ElectricianCharge.cs
+++ project-colossus/Assets/Scripts/Electrician/ElectricianCharge.cs
@@ -3,13 +3,83 @@ using System.Collections;
 
 public class ElectricianCharge : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public int m_controller;
+	public int m_startupTime;
+	public int m_activeTime;
+	public int m_cooldownTime;
+
+	public int energyGained;
+
+	private bool absorbedEnergy;
 	
+	private AbilityTimer timer;
+	public AbilityState state { get; set; }
+	
+	private CharacterStateController stateController;
+	private CharacterMovement characterMovement;
+	private CharacterAttributes characterAttributes;
+	private PlayerInput playerInput;
+	public int button = 3;
+	private SpriteRenderer sr;
+	
+	// Use this for initialization
+	void Start()
+	{
+		timer = new AbilityTimer( m_startupTime, m_activeTime, m_cooldownTime );
+		stateController = (CharacterStateController)GetComponent<CharacterStateController>();
+		characterMovement = (CharacterMovement)GetComponent<CharacterMovement>();
+		characterAttributes = (CharacterAttributes)GetComponent<CharacterAttributes>();
+		state = AbilityState.Inactive;
+		playerInput = InputManager.Players[m_controller];
+		sr = (SpriteRenderer)GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		timer.Update();
+		state = timer.State;
+		switch (state) {
+		case AbilityState.Startup:
+			break;
+
+		case AbilityState.Active:
+			if (!absorbedEnergy) {
+				ElectricianTowerResourceCounter tower;
+				tower = (ElectricianTowerResourceCounter)GetComponent<ElectricianTowerResourceCounter> ();
+				if (tower.resource >= energyGained) {
+					tower.resource -= energyGained;
+					characterAttributes.ModifyResource (energyGained);
+
+					absorbedEnergy = true;
+				}
+			}
+			break;
+
+		case AbilityState.Cooldown:		
+			break;
+
+		default:
+			break;
+		}
+		if( timer.IsOver )
+		{
+			AbilityEnd();
+		}
+	}
 	
+	public int GetButton()
+	{
+		return button;
+	}
+
+	public void AbilityStart() {
+		timer.Start ();
+	}
+
+	public void AbilityEnd() {
+		if (!playerInput.Abilities [button]) {
+			stateController.EndAbilities();
+			state = AbilityState.Inactive;
+		}
 	}
 }
---
+++ project-colossus/Assets/Scripts/Electrician/ElectricianTowerResourceCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricianTowerResourceCounter : MonoBehaviour {
+
+	private int resource = 0;
+	private double elapsedTime = 0;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		elapsedTime += Time.deltaTime;
+
+		if (elapsedTime > 1.0) {
+			resource += 2;
+			elapsedTime = 1;
+		}
+	}
+}
